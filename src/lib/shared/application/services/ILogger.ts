d8685d10fc0725de24412a2d7f095cddc89709ba
Leandrho <leandro.coder01@gmail.com>
2025-08-19 16:32:47 -0300

    add interface for logger service

---
+++ src/lib/shared/application/services/ILogger.ts
@@ -0,0 +1,8 @@
+
+export interface ILogger {
+    info(message: string, context?: Record<string, any>): void;
+    warn(message: string, context?: Record<string, any>): void;
+    error(message: string, error?: Error, context?: Record<string, any>): void;
+    debug(message: string, context?: Record<string, any>): void;
+    http(message: string, context?: Record<string, any>): void;
+}
