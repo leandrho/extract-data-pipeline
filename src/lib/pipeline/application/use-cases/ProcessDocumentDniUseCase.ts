2555a8097b139453020bf4377b37578802d1732f
Leandrho <leandro.coder01@gmail.com>
2025-08-19 16:33:39 -0300

    change uc

--- src/lib/pipeline/application/use-cases/ProcessDocumentDniUseCase.ts
+++ src/lib/pipeline/application/use-cases/ProcessDocumentDniUseCase.ts
@@ -1,6 +1,7 @@
 import { OcrService } from 'src/lib/shared/application/services/OcrService';
 import { DataExtractService } from '../../domain/services/DataExtractService';
 import { DocumentDniOutDto } from '../dtos/DocumentDniOutDto';
+import { ILogger } from 'src/lib/shared/application/services/ILogger';
 
 
 export class ProcessDocumentDniUseCase {
@@ -8,6 +9,7 @@ export class ProcessDocumentDniUseCase {
     constructor(
         private readonly dataExtractor: DataExtractService,
         private readonly ocrService: OcrService,
+        private readonly logger: ILogger
     ){}
 
     async execute(imgPath: string): Promise<DocumentDniOutDto | null> {
