6351addfe96b9a7068f382f1a137846f2923cd98
Leandrho <leandro.coder01@gmail.com>
2025-08-21 17:01:10 -0300

    add router

--- src/lib/pipeline/infrastructure/http/PipeRouter.ts
+++ src/lib/pipeline/infrastructure/http/PipeRouter.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
 import { PipeController } from "./PipeController";
+import { IFileUpload } from '../../../shared/application/interfaces/IFileUpload';
 
 export class PipeRouter {
-    public readonly router: Router;
-    constructor( private readonly pipeController: PipeController ) {
-        this.router = Router();
+    
+    private readonly _router: Router;
+
+    constructor( private readonly fileUpload: IFileUpload, private readonly pipeController: PipeController ) {
+        this._router = Router();
         this.initializeRoutes();
     }
     private initializeRoutes(): void {
-        this.router.post('/', (req, res) => this.pipeController.processDNI(req, res));
+        this._router.post('/single', this.fileUpload.single('file'), (req, res) => this.pipeController.processDNI(req, res));
+        this._router.post('/multiple', this.fileUpload.multiple('file', 2), (req, res) => this.pipeController.processDNI(req, res));
+    }
+
+    public get router(): Router{
+        return this._router;
     }
 
 }
