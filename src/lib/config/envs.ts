d8906708a84e86d177809c8eeff9077938e31565
Leandrho <leandro.coder01@gmail.com>
2025-08-28 09:43:28 -0300

    ...

--- src/lib/config/envs.ts
+++ src/lib/config/envs.ts
@@ -9,6 +9,8 @@ const envs = {
     LOG_LEVEL: env.get('LOG_LEVEL').default('info').asString(),
     NODE_ENV: env.get('NODE_ENV').default('development').asString(),
     PORT: env.get('PORT').default(3000).asPortNumber(),
+    OPENAI_API_KEY: env.get('OPENAI_API_KEY').required().asString(),
+    OPENAI_BASE_URL: env.get('OPENAI_BASE_URL').asString(),
 }
 
 export default envs;
